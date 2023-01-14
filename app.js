9a006352cef3c7bb45d4c547caeefbf5674cdbf3
gordonbrice <gordon@brice4.com>
2023-01-14 15:53:35 -0500

    Added client and server.

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 const { ethers } = require("ethers");
-//const provider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/015df1be12b048868f4207cab21cb8f9");
 
-//const blockNumber = async() => {
-//    console.log(await provider.getBlockNumber());
-//}
-
-//blockNumber();
 
 const provider = new ethers.providers.WebSocketProvider("wss://mainnet.infura.io/ws/v3/015df1be12b048868f4207cab21cb8f9");
 
 provider.on('block', (blockNumber) => {
-    console.log('New Block: ' + blockNumber);
+    console.log('Block: ' + blockNumber);
 
-    const gas = async() => {
+    const gas = async () => {
         console.log('Gas:' + await provider.getGasPrice());
     }
     gas();
 
-    const block = async() => {
+/*     const block = async() => {
         console.log(await provider.getBlock());
     }
     block();
-  });
+ */  });
