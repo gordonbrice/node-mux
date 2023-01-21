270c5ee89f667537cb0231db3fb4f1dfa914b668
gordonbrice <gordon@brice4.com>
2023-01-21 17:47:27 -0500

    Deployed to Azure.

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const WebSocket = require('ws');
 const { ethers } = require("ethers");
-const sql = require('mssql');
-const wss = new WebSocket.Server({ port: 8080 });
-require('dotenv').config();
+//const sql = require('mssql');
+const port = process.env.PORT || 8080;
+const wss = new WebSocket.Server({ port: port });
+//require('dotenv').config();
+//const { DotenvAzure } = require('dotenv-azure');
+
 
 wss.on('connection', ws => {
-    //var fs = require('fs');
-    //var keystore = JSON.parse(fs.readFileSync('keystore.json', 'utf8'));
-
-    console.log(`URL: ${process.env.INFURA_WSS}`);
-
+    var fs = require('fs');
+    var keystore = JSON.parse(fs.readFileSync('keystore.json', 'utf8'));
+    console.log(keystore.value);
+    console.log(port);
+    ws.send(`Listening on port: ${port}`)
     ws.on('message', message => {
 
-        var provider = new ethers.providers.WebSocketProvider(process.env.INFURA_WSS);
+        var provider = new ethers.providers.WebSocketProvider(keystore.value);
 
         console.log(`Received message: ${message}`);
 
